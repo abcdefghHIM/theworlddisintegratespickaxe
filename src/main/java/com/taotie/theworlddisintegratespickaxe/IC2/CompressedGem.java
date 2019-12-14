3448de6c6e75cea32274e29a87d0d79295922c3b
abcdefghHIM <1404615410@qq.com>
2019-12-14 11:00:32 +0800

    重构的新项目！

---
+++ src/main/java/com/taotie/theworlddisintegratespickaxe/IC2/CompressedGem.java
@@ -0,0 +1,55 @@
+package com.taotie.theworlddisintegratespickaxe.IC2;
+
+import ic2.api.recipe.ICannerBottleRecipeManager;
+import ic2.api.recipe.IRecipeInput;
+import ic2.api.recipe.MachineRecipe;
+import ic2.api.recipe.MachineRecipeResult;
+import ic2.api.recipe.RecipeOutput;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+
+public class CompressedGem implements ICannerBottleRecipeManager{
+
+	@Override
+	public boolean addRecipe(Input input, ItemStack output, NBTTagCompound metadata, boolean replace) {
+		// TODO 自动生成的方法存根
+		return false;
+	}
+
+	@Override
+	public MachineRecipeResult<Input, ItemStack, RawInput> apply(RawInput input, boolean acceptTest) {
+		// TODO 自动生成的方法存根
+		return null;
+	}
+
+	@Override
+	public Iterable<? extends MachineRecipe<Input, ItemStack>> getRecipes() {
+		// TODO 自动生成的方法存根
+		return null;
+	}
+
+	@Override
+	public boolean isIterable() {
+		// TODO 自动生成的方法存根
+		return false;
+	}
+
+	@Override
+	public boolean addRecipe(IRecipeInput container, IRecipeInput fill, ItemStack output, boolean replace) {
+		// TODO 自动生成的方法存根
+		return false;
+	}
+
+	@Override
+	public void addRecipe(IRecipeInput container, IRecipeInput fill, ItemStack output) {
+		// TODO 自动生成的方法存根
+		
+	}
+
+	@Override
+	public RecipeOutput getOutputFor(ItemStack container, ItemStack fill, boolean adjustInput, boolean acceptTest) {
+		// TODO 自动生成的方法存根
+		return null;
+	}
+
+}
